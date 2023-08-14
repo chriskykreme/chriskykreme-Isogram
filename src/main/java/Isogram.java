498598e82b5cb6f78a530f092f7cfbbc737f6ebb
Christopher King <king.cdk@gmail.com>
2023-08-14 00:38:59 +0000

    added logic to Isogram file

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Isogram {
     /**
      * An Isogram is a word where no letters are repeated. You may assume that there will only be lowercase
@@ -13,6 +13,30 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        boolean isIsogram = true;
+        // Create and populate a HashMap of alphabetic characters, and number of occureneces in string
+        Map<Character, Integer> alphabet = new HashMap<Character, Integer>(); 
+        for (char ch = 'a'; ch <= 'z'; ++ch) {
+            alphabet.put(ch, 0);
+        }
+
+        // Add number of occurences per letter to the HashMap
+        for (int i = 0; i <str.length(); i++) {
+            char c = str.charAt(i);
+            int val = alphabet.get(c);
+            alphabet.put(c, val+1);
+        }
+
+        // Retrieve list of keys and if the number of any occurence is > 1
+        // it is not an isogram so return false
+        Collection<Integer> values = alphabet.values();
+        for (Integer value : values) {
+            if (value > 1) {
+                isIsogram = false;
+            }
+        }
+        
+        return isIsogram;
+
     }
 }
